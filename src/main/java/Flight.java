62f003e123f982640c4db3fb3fdaafdf070f300d
Saima Miah <saima_miah@hotmail.co.uk>
2023-10-24 12:09:13 +0100

    flight class with array list and methods

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+
 public class Flight {
+
+    private ArrayList<Passenger> passengersOnboard;
+
+
+    public Flight() {
+        this.passengersOnboard = new ArrayList<Passenger>();
+    }
+
+    public ArrayList<Passenger> getPassengersOnboard() {
+        return passengersOnboard;
+    }
+
+    public void setPassengersOnboard(ArrayList<Passenger> passengersOnboard) {
+        this.passengersOnboard = passengersOnboard;
+    }
+
+    public void modifyPassengersOnboard(Passenger passenger) {
+        this.passengersOnboard.add(passenger);
+    }
 }
+
+
